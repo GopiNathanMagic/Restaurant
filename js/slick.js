d1517a0ed0dbca7307bbae1591b19ce7ca62750b
Gopinathan S <151734545+GopiNathanMagic@users.noreply.github.com>
2024-02-23 17:42:07 +0530

    pop up update
    pop up update

--- js/slick.js
+++ js/slick.js
@@ -20,8 +20,74 @@ $(document).ready(function () {
   //   $.scrollTo($("#myproduct-scroll"), 1000);
   // });
 
-  //   $('#myproduct').click(function(event) {
-  //     event.preventDefault();
-  //   $.scrollTo($('#myproduct-scroll'), 1000);
+  // $("#myButton").click(function (event) {
+  //   event.preventDefault();
+  //   $.scrollTo($("#myDiv"), 1000);
   // });
+
+  /* pop up */
+  function deselect(e) {
+    $(".pop").slideFadeToggle(function () {
+      e.removeClass("selected");
+    });
+  }
+
+  $(function () {
+    $("#contact").on("click", function () {
+      if ($(this).hasClass("selected")) {
+        deselect($(this));
+      } else {
+        $(this).addClass("selected");
+        $(".pop").slideFadeToggle();
+      }
+      return false;
+    });
+
+    $(".close").on("click", function () {
+      deselect($("#contact"));
+      return false;
+    });
+  });
+
+  $.fn.slideFadeToggle = function (easing, callback) {
+    return this.animate(
+      { opacity: "toggle", height: "toggle" },
+      "fast",
+      easing,
+      callback
+    );
+  };
+
+  // /* pop up 1 */
+  // function deselect(e) {
+  //   $(".pop").slideFadeToggle(function () {
+  //     e.removeClass("selected");
+  //   });
+  // }
+
+  // $(function () {
+  //   $("#contact").on("click", function () {
+  //     if ($(this).hasClass("selected")) {
+  //       deselect($(this));
+  //     } else {
+  //       $(this).addClass("selected");
+  //       $(".pop").slideFadeToggle();
+  //     }
+  //     return false;
+  //   });
+
+  //   $(".close").on("click", function () {
+  //     deselect($("#contact"));
+  //     return false;
+  //   });
+  // });
+
+  // $.fn.slideFadeToggle = function (easing, callback) {
+  //   return this.animate(
+  //     { opacity: "toggle", height: "toggle" },
+  //     "fast",
+  //     easing,
+  //     callback
+  //   );
+  // };
 });
